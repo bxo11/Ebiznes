f99010271866392d218d120b9a6ac0fe71fd0d18
bxo11 <krzysiek.klim1999@gmail.com>
2023-05-21 18:04:30 +0200

    Init proj 4

---
+++ 4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"awesomeProject/endpoints"
+	"awesomeProject/models"
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+)
+
+func main() {
+	// Connect to the SQLite database
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Auto-migrate the Product model
+	db.AutoMigrate(&models.Category{})
+	db.AutoMigrate(&models.Product{})
+	db.AutoMigrate(&models.Cart{})
+
+	// Create a new Echo instance
+	e := echo.New()
+
+	// Initialize the endpoints with the database connection
+	endpoints.InitProductEndpoints(db, e)
+	endpoints.InitCartEndpoints(db, e)
+
+	// Start the server
+	e.Start(":8080")
+}
